52ad0baa00a95c74f525bf371247e7e3c02cbe40
Muhammet Emirhan <158741458+memirhan@users.noreply.github.com>
2025-08-02 00:26:07 +0300

    Update dinleyici.py

--- dinleyici.py
+++ dinleyici.py
@@ -10,7 +10,6 @@ class Baglama:
 
         self.baglanti.bind((ip,port))
         self.baglanti.listen(5)
-        print("https://github.com/memirhan")
         print("Dinlemeye Başladı ")
 
         self.baglan,adres = self.baglanti.accept()
